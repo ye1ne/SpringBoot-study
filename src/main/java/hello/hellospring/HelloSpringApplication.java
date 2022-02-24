4b351d2a91ffd8a6f6b9dac43b873eb5e27ab34e
Yewon Jeong <yewon7036@gmail.com>
2022-02-24 19:07:19 +0900

    섹션 5.회원 관리 예제 - 웹 MVC 개발

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package hello.hellospring;
+package hello.hellospring; //요기 하위 패키지는 컴포넌트 스캔함 , 위에 패키지는 스캔되지않음
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,7 +1,6 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
